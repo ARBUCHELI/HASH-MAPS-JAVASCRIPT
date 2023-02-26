9ec30d5b65430f48cc581733c3b3e4a1b94012a4
ARBUCHELI <radio_seattle@hotmail.com>
2023-02-26 17:43:12 -0500

    Hashing

--- HashMap.js
+++ HashMap.js
@@ -3,6 +3,15 @@ class HashMap {
     this.hashmap = new Array(size)
       .fill(null);
   }
+
+  hash(key) {
+    let hashCode = 0;
+    for (let i = 0; i < key.length; i++) {
+      hashCode += hashCode + key.charCodeAt(i);
+    }
+    return hashCode;
+  }
+
 }
 
 module.exports = HashMap;
